819fd1ba32e6c1d69d25c4fd48d7b3c45fef699f
AndrewFriend8845 <Andrew.Friend8845@Butlertech.org>
2020-08-24 08:45:10 -0400

    3 wire leds

--- Incident_Counter/display_alt.py
+++ Incident_Counter/display_alt.py
@@ -14,7 +14,7 @@ def off():
 def on(lightNum):
     for i in range(3):
         lightNum = (lightNum*3) + i
-        leds[lightNum] = 0x100000
+        leds[lightNum] = (0, 255, 0, 0, 0)
 
 
 def setDisplay(number):
