b2fe34441c18da3a35a7f9c0e75cd68ade4ea662
Drew-Friend <dfriend414@gmail.com>
2020-10-28 15:39:04 +0000

    Zach is dum

--- Incident_Counter/display_alt.py
+++ Incident_Counter/display_alt.py
@@ -99,7 +99,7 @@ def rainbow():
                 leds[j] = (0,0,0)
             
             leds.show()
-            time.sleep(0.01)
+            time.sleep(0.001)
 #Wheel is used by the rainbow to...rainbow
 def wheel(pos):
     if pos < 0 or pos > 255:
